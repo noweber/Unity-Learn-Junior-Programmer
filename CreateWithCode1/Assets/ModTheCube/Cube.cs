4906db27090c8e6f29d41e0f11f1a7681392cc5e
Nicholas Owen Weber <noweber@gmail.com>
2023-02-14 10:02:16 -0800

    Primitives Project Created

--- CreateWithCode1/Assets/ModTheCube/Cube.cs
+++ CreateWithCode1/Assets/ModTheCube/Cube.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+
+    public Vector3 RandomPosition;
+
+    public Color RandomColor;
+
+    private void Awake()
+    {
+        if(RandomColor == new Color(0, 0, 0, 0))
+        {
+            RandomColor = new Color(
+            Random.Range(0, 1.0f),
+            Random.Range(0, 1.0f),
+            Random.Range(0, 1.0f),
+            1.0f
+            );
+        }
+
+        if(RandomPosition == Vector3.zero)
+        {
+            RandomPosition = new Vector3(
+            Random.Range(0, 4),
+            Random.Range(0, 4),
+            Random.Range(0, 4)
+            );
+        }
+    }
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
+        transform.position = RandomPosition;
         transform.localScale = Vector3.one * 1.3f;
-        
+
         Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+
+        material.color = RandomColor;
     }
-    
+
     void Update()
     {
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
