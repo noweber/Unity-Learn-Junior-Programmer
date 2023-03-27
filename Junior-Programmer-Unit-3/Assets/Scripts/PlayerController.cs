46f5b3f91fc9db6ec9026060901232a15eb4b6fa
Nicholas Owen Weber <noweber@gmail.com>
2023-03-26 21:33:16 -0700

    Unit 3.4 Code

--- Junior-Programmer-Unit-3/Assets/Scripts/PlayerController.cs
+++ Junior-Programmer-Unit-3/Assets/Scripts/PlayerController.cs
@@ -10,11 +10,21 @@ public class PlayerController : MonoBehaviour
 
     public bool IsGameOver;
 
+    public ParticleSystem ExplosionParticles;
+
+    public ParticleSystem DirtParticles;
+
+    public AudioClip JumpSound;
+
+    public AudioClip CrashSound;
+
     private Rigidbody playerRigidbody;
 
     private bool isOnGround;
 
     private Animator playerAnimator;
+
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +34,19 @@ public class PlayerController : MonoBehaviour
         isOnGround = true;
         IsGameOver = false;
         playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !IsGameOver)
         {
             isOnGround = false;
             playerRigidbody.AddForce(Vector3.up *  JumpForce, ForceMode.Impulse);
             playerAnimator.SetTrigger("Jump_trig");
+            DirtParticles.Stop();
+            playerAudio.PlayOneShot(JumpSound, 1.0f);
         }
     }
 
@@ -42,10 +55,14 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            DirtParticles.Play();
         } else if(collision.gameObject.CompareTag("Obstacle")) {
             IsGameOver = true;
             playerAnimator.SetBool("Death_b", true);
             playerAnimator.SetInteger("DeathType_int", 1);
+            ExplosionParticles.Play();
+            DirtParticles.Stop();
+            playerAudio.PlayOneShot(CrashSound, 1.0f);
         }
     }
 }
